d23554302b9e63434a941b7338875591e9f6de72
krits2503 <khandelwalkriti9925@gmail.com>
2020-12-18 15:35:38 +0530

    update

---
+++ trees/rahul2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class rahul2 {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        String str=scn.next();
+        String []s=str.split("-");
+        for(int i=0;i<s.length;i++)
+            System.out.println(s[i]);
+    }
+}
---
+++ trees/rahull.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class rahull {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        String str=scn.nextLine();
+       
+    int c=0,count=0;
+            int start=0;
+            boolean flag=false;
+            HashMap<String,Integer>map=new HashMap<>();
+        for(int i=1;i<str.length();i++)
+        {
+            char ch=str.charAt(i);
+            char ch1=str.charAt(i-1);
+            if(Character.isDigit(ch)&& c==0 && ch1=='-')
+           { flag=true;
+            start=i;
+           }
+            if(Character.isDigit(ch))
+               c++;
+            else c=0;
+            
+            
+            if(c==4 && flag )
+            {
+                String st=str.substring(start,i+1);
+               
+                if(!map.containsKey(st))
+                { map.putIfAbsent(st,1);
+                    count++;}
+                flag=false;
+            }
+        }
+        System.out.println(count);
+    }
+}
