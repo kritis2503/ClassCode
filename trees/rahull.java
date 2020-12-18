d042596aea440883a4064b1d0226c0b99a86b867
krits2503 <khandelwalkriti9925@gmail.com>
2020-12-18 16:39:22 +0530

    update

--- trees/rahull.java
+++ trees/rahull.java
@@ -8,10 +8,10 @@ public class rahull {
             int start=0;
             boolean flag=false;
             HashMap<String,Integer>map=new HashMap<>();
-        for(int i=1;i<str.length();i++)
-        {
-            char ch=str.charAt(i);
+        for(int i=1;i<str.length()-1;i++)
+        {char ch=str.charAt(i);
             char ch1=str.charAt(i-1);
+            char ch2=str.charAt(i+1);
             if(Character.isDigit(ch)&& c==0 && ch1=='-')
            { flag=true;
             start=i;
@@ -19,15 +19,13 @@ public class rahull {
             if(Character.isDigit(ch))
                c++;
             else c=0;
-            
-            
+            if(Character.isDigit(ch2))
+               flag=false; 
             if(c==4 && flag )
             {
                 String st=str.substring(start,i+1);
-               
-             map.putIfAbsent(st,1);
-                    
-                flag=false;
+               map.putIfAbsent(st,1);
+            flag=false;
             }
         }
         System.out.println(map.size());
